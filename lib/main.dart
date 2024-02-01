843ff4218238ec437f436dc16ff151dbd8a08dd5
Rohit Rana <rkrana6631@gmail.com>
2024-02-01 12:29:06 +0530

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:tpworld_admin/firebase_options.dart';
 import 'package:tpworld_admin/utils/colors.dart';
 
 import 'routes.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MyApp());
 }
 
